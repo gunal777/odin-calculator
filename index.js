c12692fb968664abe0e66d3f57623b78dae8995f
Gunal <gunalb20@gmail.com>
2025-09-06 23:06:02 +0530

    functional

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 const display = document.getElementById("display");
+let num1, num2;
+let opr;
+
+function assign(input)
+{
+    console.log(input);
+    if(!isNaN(input)|| input === '.')
+    {
+        display.value += input; 
+    }
+    else if(['+','-','*','/'].includes(input))
+    {
+        if(display.value !== '')
+            {
+                num1 = parseFloat(display.value);
+                opr = input;
+                display.value = '';
+            }
+    }
+    else if(input === "C")
+    {
+        display.value = "";
+        num1 = undefined;
+        num2 = undefined;
+        opr = undefined;
+    }
+    else if(input === '=')
+    {
+        result();
+    }
+}
+
+
 function add(a,b)
 {
     return a+b;
@@ -31,28 +64,31 @@ function operate(a,b,c)
     {
         case '+':
             return add(a,b);
-        break;
         case '-':
             return substract(a,b);
-        break;
         case '*':
             return multiply(a,b);
-        break;
         case '/':
             return divide(a,b);
-        break;
         default:
-            return;
-        break;
+            return "Error";
     }
 }
 
-function appendToDisplay(input)
+function result()
 {
-    display.value += input; 
+    if(num1 !== undefined && opr !== undefined && display.value !=="")
+    {
+        num2 = parseFloat(display.value);
+        let result = operate(num1,num2,opr)
+        display.value = result;
+
+        num1 = undefined;
+        num2 = undefined;
+        opr = undefined;
+    }
 }
 
-function cleardisplay()
-{
-    display.value = "";
+function backspace() {
+    display.value = display.value.slice(0, -1);
 }
